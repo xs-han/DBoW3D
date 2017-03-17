7c3881ba2a2f51d15756e34be8293874b4f3a58e
shenuncle <hanxushen1105@sina.com>
2017-03-17 21:54:57 +0800

    Initialize the project. Add DBoW3 files into it.

---
+++ src/VirtualView.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : VirtualView.cpp
+// Author      : Xushen Han
+// Version     :
+// Copyright   : Xushen from SJTU
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+//#include "./DBoW3/DBoW3.h"
+//#include "./DBoW3/DescManip.h"
+using namespace std;
+//using namespace DBoW3;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
